a7f0f48f563499bea0bbf3c8a00ddda22f939193
ajoa2757 <ajoa4936@gmail.com>
2022-09-18 20:39:19 +0900

    Added annotations
    about URL parse

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+//require functions are used like #includes from C/C++.
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+
+    //if you look up on the properties of this, _url is a string.
+    var _url = request.url;
+
+    //this parse function from pakage url.
+    //it takes a string and parses it. return it as 'parsed' URL object. no idea what it is.
+    //query is thus one of the fields of Object URL.
+    var queryData = url.parse(_url, true).query;
+
+
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
